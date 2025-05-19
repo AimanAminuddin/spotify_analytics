70ef1cba80637100de6309f0bec2f9efb36fcba2
Aiman Aminuddin <aimanaminuddin922@gmail.com>
2025-05-19 15:23:58 +0800

    convert code for plotting spotify trends into OOP format

--- DashboardBuild.py
+++ DashboardBuild.py
@@ -1,14 +1,8 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px
-import streamlit as st
-
-# Package to generate data
-# needed to build dashboard
-from DataBuilder import (
-    DataBuilder,
-    load_spotify_credentials
-)
+import numpy as np
+import pandas as pd
+from wordcloud import WordCloud
 
 class DashboardBuild:
     '''
@@ -55,8 +49,121 @@ class DashboardBuild:
         plt.xlabel("Artist")
         plt.ylabel("Count")
         plt.xticks(rotation=45)
-        
+
         plt.show()
+
+        return
+
+    def plot_top_album(self):
+        '''
+        Plot the total number of songs by same
+        album in saved & liked songs
+        '''
+
+        top_albums = self.dashboard_data['album_name'].value_counts().head(10)
+        plt.figure(figsize=(10, 6))
+        sns.barplot(x=top_albums.index, y=top_albums.values, palette='Blues')
+        plt.title("Top 10 Albums by Track Count")
+        plt.xlabel("Album")
+        plt.ylabel("Track Count")
+        plt.xticks(rotation=45)
+        plt.show()
+        
+        return
+    
+    def plot_explicit_non_explicit(self):
+        '''
+        Plot the total number of songs that 
+        are explicit against songs that are not
+        '''
+
+        explicit_counts = (
+            self.dashboard_data['explicit']
+                .value_counts()
+                .rename({True: 'Explicit', False: 'Clean'})
+        )
+
+        plt.figure(figsize=(8, 8))
+        sns.countplot(x='explicit', data= explicit_counts, palette='Blues')
+        plt.title("Explicit vs Clean Tracks")
+        plt.xlabel("Track Type")
+        plt.ylabel("Count")
+        plt.xticks([0, 1], ['Clean', 'Explicit'])
+        plt.show()
+
+        return
+
+    def plot_track_distribution(self):
+        '''
+        Function that generates the distribution 
+        of song durations in liked songs 
+        '''
+
+        # Track Duration Distribution
+        self.dashboard_data['duration_min'] = np.round(self.dashboard_data['duration_ms']/60000,2)
+        plt.figure(figsize=(10, 6))
+        sns.histplot(self.dashboard_data['duration_min'], kde=True, color='skyblue', bins=30)
+        plt.title("Track Duration Distribution")
+        plt.xlabel("Duration (minutes)")
+        plt.ylabel("Frequency")
+        plt.show()
+
+        return
+    
+    def plot_release_year_trend(self):
+        '''
+        Function that generaets line chart 
+        showing number of track(s) based on 
+        release year in saved playlist
+        '''
+
+        # Release Year Trend
+        self.dashboard_data['release_date'] = pd.to_datetime(
+            self.dashboard_data['release_date'], 
+            format='mixed', 
+            errors='coerce'
+            )
+
+        self.dashboard_data['release_year'] = self.dashboard_data['release_date'].dt.year
+        yearly_counts = self.dashboard_data['release_year'].value_counts().sort_index()
+        plt.figure(figsize=(10, 6))
+        sns.lineplot(x=yearly_counts.index, y=yearly_counts.values, marker='o', color='b')
+        plt.title("Number of Tracks Released Over Years")
+        plt.xlabel("Year")
+        plt.ylabel("Number of Tracks")
+        plt.show()
+
+        return 
+    
+    def generate_word_cloud(self):
+        '''
+        Function that generaets world cloud 
+        based on saved & like songs
+        '''
+
+        # Genre Distribution (if available and contains genres)
+        if 'genres' in self.dashboard_data.columns and self.dashboard_data['genres'].notna().any():
+            all_genres = ', '.join(self.dashboard_data['genres'].dropna())
+
+            # Check if there's any genre to create a word cloud
+            if all_genres.strip():  # Ensure there is at least one genre
+                wordcloud = WordCloud(
+                    width=800, height=400,
+                    background_color='white'
+                    ).generate(all_genres)
+
+                plt.figure(figsize=(10, 5))
+                plt.imshow(wordcloud, interpolation='bilinear')
+                plt.axis('off')
+                plt.title("Genre Word Cloud")
+                plt.show()
+
+            else:
+                print("No genres available to generate word cloud.")
+
+        else:
+            print("No 'genres' column or genres data is missing.")
+
         return
 
 
